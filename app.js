11f74cdefe8400c05f427072a94b902af5ad54a6
locnguyenfastboy <loc_nguyen@fastboy.net>
2024-11-08 00:54:57 -0600

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// Dark mode toggle
+const themeSwitchCheckbox = document.querySelector('.theme-switch__checkbox');
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    themeSwitchCheckbox.checked = true;
+}
+themeSwitchCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('service-worker.js')
