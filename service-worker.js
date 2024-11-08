6d796514a902b07b6f57f961b4d4d13d86d06bf6
locnguyenfastboy <loc_nguyen@fastboy.net>
2024-11-08 01:16:05 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,8 @@ self.addEventListener('install', (event) => {
                 '/index.html',
                 '/styles.css',
                 '/app.js',
-                'https://script.google.com/macros/s/AKfycbx8iUDiceaU81rtl9d4wthZKIYX3_GsRbFV33KxHCmwvIQOKxxmkpmyCSogfX1431gO_w/exec' // Caching the iframe content may not work due to CORS
+                '/style-darkmode.css',
+                '/toggle-switches.css',
             ]);
         })
     );
